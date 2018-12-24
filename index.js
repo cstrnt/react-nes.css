c9c694ec343036f4efec2733bd2f4d3d1f01ef7b
Tim Raderschad <tim.raderschad@gmail.com>
2018-12-24 12:02:51 +0100

    fix import

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import Icon from "./components/Icon";
 import Input from "./components/Input";
 import List from "./components/List";
 import Nes from "./components/Nes";
-import Progress from "./components";
+import Progress from "./components/Progress";
 import Radio from "./components/Radio";
 import Table from "./components/Table";
 import Textarea from "./components/Textarea";
