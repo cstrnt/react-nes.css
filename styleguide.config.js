efbe495fbacfc8ea60ca83452ccd565f3b1e24ec
Tim Raderschad <tim.raderschad@gmail.com>
2018-12-24 13:07:25 +0100

    Add docs

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-    webpackConfig: {
-      module: {
-        rules: [
-          // Babel loader, will use your project’s .babelrc
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-          },
-          // Other loaders that are needed for your components
-          {
-            test: /\.css$/,
-            loader: 'style-loader!css-loader?modules'
-          }
-        ]
-      }
+  title: "react-nes.css Docs",
+  styleguideDir: "docs",
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
     }
   }
+};
