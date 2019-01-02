6c4b11d2979f6c6bafc25fbf8f9cbff05de4b8a0
Tim Raderschad <tim.raderschad@gmail.com>
2019-01-02 09:46:26 +0100

    Update tests

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import classNames from "classnames";
-import PropTypes from "prop-types";
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 const Avatar = ({ size, rounded, ...props }) => {
-  const className = classNames("nes-avatar", size && `is-${size}`, {
-    "is-rounded": rounded
+  const className = classNames('nes-avatar', size && `is-${size}`, {
+    'is-rounded': rounded,
   });
   return <img className={className} {...props} />;
 };
 
 Avatar.propTypes = {
-  size: PropTypes.oneOf(["small", "medium", "large"]),
-  rounded: PropTypes.bool
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  rounded: PropTypes.bool,
 };
 
 export default Avatar;
