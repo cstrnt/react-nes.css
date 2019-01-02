813f0b452c6a279d9ac0993a191ca870d2bc5cae
Tim Raderschad <tim.raderschad@gmail.com>
2019-01-02 09:04:07 +0100

    fix path

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import Avatar from "./components/Avatar";
-import Balloon from "./components/Balloon";
-import Button from "./components/Button";
-import Checkbox from "./components/Checkbox";
-import Container from "./components/Container";
-import Icon from "./components/Icon";
-import Input from "./components/Input";
-import List from "./components/List";
-import Nes from "./components/Nes";
-import Progress from "./components/Progress";
-import Radio from "./components/Radio";
-import Table from "./components/Table";
-import Textarea from "./components/Textarea";
+import Avatar from "./src/components/Avatar";
+import Balloon from "./src/components/Balloon";
+import Button from "./src/components/Button";
+import Checkbox from "./src/components/Checkbox";
+import Container from "./src/components/Container";
+import Icon from "./src/components/Icon";
+import Input from "./src/components/Input";
+import List from "./src/components/List";
+import Nes from "./src/components/Nes";
+import Progress from "./src/components/Progress";
+import Radio from "./src/components/Radio";
+import Table from "./src/components/Table";
+import Textarea from "./src/components/Textarea";
 
 module.exports = {
   Avatar,
